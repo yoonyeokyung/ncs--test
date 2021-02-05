3760243d0f577250657615fcbfbf4806cbb2cc1b
tj2 <tj2@DESKTOP-64T8B5K>
2021-02-05 17:27:52 +0900

    재시험

---
+++ ncs-test-yoonyeokyung/src/com/greedy/ncs/product/run/Applicaiton.java
@@ -0,0 +1,11 @@
+package com.greedy.ncs.product.run;
+
+import com.greedy.ncs.product.model.dto.ProductDTO;
+
+public class Applicaiton {
+
+	public static void main(String[] args) {
+		new ProductDTO();
+	}
+
+}
